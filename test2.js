e78f109646f8a21428695820ad31c227873a49aa
Unknown <evgeny.mizulin@gmail.com>
2017-05-19 12:03:41 +0300

    https

--- test2.js
+++ test2.js
@@ -18,14 +18,15 @@ appTwo.use('/', (req, res) => res.status(200).send('i am app two'))
 evh.register('photo-mosaic.ru', appOne)
 evh.register('api.photo-mosaic.ru', appTwo)
 
-server.listen(80)
+// server.listen(80)
 
-// const lex = letsencrypt.create({
-//     server: prod ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging',
-//     email: email,
-//     agreeTos: true,
-//     approveDomains: apps.map(item => item.host),
-//     app: server
-// })
-//
-// lex.listen(80, 443)
+const lex = letsencrypt.create({
+    // server: prod ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging',
+    server: 'staging',
+    email: 'evgeny.mizulin@gmail.com',
+    agreeTos: true,
+    approveDomains: ['www.photo-mosaic.ru', 'photo-mosaic.ru', 'api.photo-mosaic.ru'],
+    app: server
+})
+
+lex.listen(80, 443)
